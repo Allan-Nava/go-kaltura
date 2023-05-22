170eb7706471e69dad24a15a1ae5fe17e3886757
Allan Nava <allannava95@gmail.com>
2023-05-22 15:23:02 +0200

    added github actions

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -1,1 +1,22 @@
 package configuration
+
+import (
+	"github.com/caarlos0/env/v6"
+)
+
+
+type Configuration struct {
+	IsDebug    bool `env:"IS_DEBUG"`
+	BaseUrl    string `env:"BASE_URL"`
+}
+
+
+func GetConfiguration() *Configuration {
+	configuration := Configuration{}
+	err := env.Parse(&configuration)
+	if err != nil {
+		panic("failed to read configuration")
+	}
+	//
+	return &configuration
+}
---
+++ docs/index.md
@@ -0,0 +1,12 @@
+---
+layout: default
+title: Home
+nav_order: 1
+description: "go Kaltura Docs"
+permalink: /
+last_modified_date: 2023-05-15T12:00:00+0000
+---
+
+# Go Kaltura sdk documentation
+
+A powerful golang sdk library for interacting with the Kaltura API easily
