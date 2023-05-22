f0d8ddaac342d93bc391610756b186bef20b3276
Allan Nava <allannava95@gmail.com>
2023-05-22 15:26:38 +0200

    added new gokaltura

--- gokaltura/gokaltura.go
+++ gokaltura/gokaltura.go
@@ -1,7 +1,26 @@
 package gokaltura
 
+import (
+	"log"
+	"github.com/Allan-Nava/go-kaltura/configuration"
+	"github.com/go-resty/resty/v2"
+)
+
 type IGoKaltura interface {
+	//
 }
 
 type gokaltura struct {
+	configuration *configuration.Configuration
+	restClient    *resty.Client
+}
+
+
+func NewGoKaltura(configuration *configuration.Configuration) IGoKaltura {
+	k := &gokaltura{
+		configuration: configuration,
+	}
+	k.restClient = resty.New()
+	//
+	return k
 }
