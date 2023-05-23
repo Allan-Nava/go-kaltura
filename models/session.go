9718a67579e4a9646d68aba0f4a7602e15cd2326
Allan Nava <allannava95@gmail.com>
2023-05-23 12:00:15 +0200

    added session models

---
+++ models/session.go
@@ -0,0 +1,35 @@
+package models
+
+
+/*
+{
+  "expiry": 0,
+  "ks": "",
+  "partnerId": 0,
+  "privileges": "",
+  "sessionType": 0,
+  "userId": ""
+}
+*/
+type KalturaSessionInfo struct {
+    Expiry int 
+	Ks string
+
+}
+
+/*
+{
+  "ks": "",
+  "objectType": "KalturaStartWidgetSessionResponse",
+  "partnerId": 0,
+  "userId": ""
+}
+*/
+type KalturaSessionResponse struct {
+	Ks string `json:"ks"`
+	ObjectType string `json:"objectType"`
+	PartnerId int `json:"partnerId"`
+	UserId string `json:"userId"`
+}
+
+
